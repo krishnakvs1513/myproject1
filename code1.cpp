eab82e3c27e66d6d0926a73b5f80a70bc316d3dc
krishna <krishna@localhost.localdomain>
2026-04-04 18:29:43 +0530

    firstcheck

---
+++ code1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Header file for input and output
+using namespace std; // Use names from the standard library
+
+int main() 
+{
+    // Output text to the console
+    cout << "Jai jai ganesh deva 1513 108\n";
+
+    return 0; // End the program successfully
+}
+
